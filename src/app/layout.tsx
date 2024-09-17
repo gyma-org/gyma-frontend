85a15a5ba20069fc7e575e98d8fa0aeead90c4b0
Reza Buzarjemehri <dev.reza21@gmail.com>
2024-09-17 20:56:22 +0330

    set pwa configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,13 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="fa-IR">
+			<head>
+        {/* Meta Tags for PWA */}
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="/icons/192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/512x512.png" />
+      </head>
 			<body className={vazirmatn.className}>{children}</body>
 		</html>
 	);
