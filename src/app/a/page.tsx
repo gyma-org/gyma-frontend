be1a88b001d103c561c086aa003a60535e54ccb7
Reza Buzarjemehri <dev.reza21@gmail.com>
2024-09-18 20:51:28 +0330

    key error

--- src/app/a/page.tsx
+++ src/app/a/page.tsx
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 
 import { Map, Favorite, Reservation, Profile } from "@/components/routes";
 
-const pages = [<Map />, <Favorite />, <Reservation />, <Profile />];
+const pages = [<Map key={0} />, <Favorite key={1} />, <Reservation key={2} />, <Profile key={3} />];
 
 const Application = () => {
   const [pageIndex, setPageIndex] = useState<number>(0);
