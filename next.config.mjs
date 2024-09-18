5c9120696714667d5e7dba7d7999b4db688b9741
Reza Buzarjemehri <dev.reza21@gmail.com>
2024-09-18 13:04:16 +0330

    disable service worker in development

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-import withPWA from 'next-pwa';
+import withPWA from "next-pwa";
 
 // PWA configuration
 const nextConfig = withPWA({
-  dest: 'public',  // Destination for PWA assets (like service worker)
-  // disable: process.env.NODE_ENV === 'development',  // Disable PWA in development mode
-  register: true,  // to register Service Worker
+  dest: "public", // Destination for PWA assets (like service worker)
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // to register Service Worker
   skipWaiting: true, // register faster
 });
 
