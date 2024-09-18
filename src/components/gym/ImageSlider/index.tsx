26cb74289ebe959d623720f5fdf8fdc296f43f49
Reza Buzarjemehri <dev.reza21@gmail.com>
2024-09-18 16:29:41 +0330

    add image slider

--- src/components/gym/ImageSlider/index.tsx
+++ src/components/gym/ImageSlider/index.tsx
@@ -1,6 +1,12 @@
+"use client";
+
 import React from "react";
 import Grid from "@mui/material/Grid2";
 import { CardMedia } from "@mui/material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+
+import "./swiper.css";
 
 const ImageSlider = () => {
   return (
@@ -13,12 +19,43 @@ const ImageSlider = () => {
         borderRadius: { xs: 0, md: 8 },
         overflow: "hidden",
       }}>
-      <CardMedia
-        component="img"
-        sx={{ position: "absolute", width: "100%", height: "100%", objectFit: "cover" }}
-        image="https://placehold.co/600x400"
-        title="place holder"
-      />
+      <Swiper
+        style={{
+          position: "absolute",
+          width: "100%",
+        }}
+        pagination={{
+          dynamicBullets: true,
+        }}
+        grabCursor={true}
+        effect={"creative"}
+        modules={[Pagination]}
+        className="mySwiper">
+        <SwiperSlide>
+          <CardMedia
+            component="img"
+            sx={{ width: "100%", height: "100%", objectFit: "cover", aspectRatio: "4 / 3" }}
+            image="https://placehold.co/600x400"
+            title="place holder"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardMedia
+            component="img"
+            sx={{ width: "100%", height: "100%", objectFit: "cover", aspectRatio: "4 / 3" }}
+            image="https://placehold.co/600x400"
+            title="place holder"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <CardMedia
+            component="img"
+            sx={{ width: "100%", height: "100%", objectFit: "cover", aspectRatio: "4 / 3" }}
+            image="https://placehold.co/600x400"
+            title="place holder"
+          />
+        </SwiperSlide>
+      </Swiper>
     </Grid>
   );
 };
