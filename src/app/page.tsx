df571684b88b76ddaeead6c2fe8c6d275c9c7677
Reza Buzarjemehri <dev.reza21@gmail.com>
2024-09-18 13:35:06 +0330

    gym page top component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import dynamic from "next/dynamic";
 const Map = dynamic(() => import("@/components/map"), { ssr: false });
 
 export default function Home() {
-	return (
-		<div className="flex relative h-screen w-screen flex-col items-center justify-between overflow-hidden">
-			<div>
-				<Map />
-			</div>
-			{/* <div className="absolute z-50 bottom-0 left-0 right-0">
+  return (
+    <div className="flex relative h-screen w-screen flex-col items-center justify-between overflow-hidden">
+      <div>
+        <Map />
+      </div>
+      {/* <div className="absolute z-50 bottom-0 left-0 right-0">
 				<NavigationBar />
 			</div> */}
-		</div>
-	);
+    </div>
+  );
 }
