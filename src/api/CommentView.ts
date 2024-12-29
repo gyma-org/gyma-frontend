93d76e7a54ba6a8283a3f8490b632c6743dd3908
bayan0303 <bay01an0303@gmail.com>
2024-12-29 13:07:44 +0000

    fixing url adress to fetch comments

--- src/api/CommentView.ts
+++ src/api/CommentView.ts
@@ -1,10 +1,10 @@
 import { CommentView } from '../types/CommentView';
-import { API_BASE_URL } from '../config';
+import { API_USER_URL } from '../config';
 // export const API_BASE_URL = 'http://127.0.0.1:8000';
 
 // Function to get gym details by gym code and user_id as query parameter
 export const getCommentsView = async (user_id: string): Promise<CommentView | null> => {
-  const url = new URL(`${API_BASE_URL}/comments/view_comments/`);
+  const url = new URL(`${API_USER_URL}/comments/view_comments/`);
   url.searchParams.append('user_id', user_id);
 
   const response = await fetch(url.toString());
