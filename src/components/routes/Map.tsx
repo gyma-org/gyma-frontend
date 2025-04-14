da00508841a03ada1a84a3466cbac20623ecc5d0
gyma-team <gymadevelopment@gmail.com>
2025-04-14 10:41:32 +0330

    fix map whitespaces

--- src/components/routes/Map.tsx
+++ src/components/routes/Map.tsx
@@ -7,32 +7,47 @@ import dynamic from "next/dynamic";
 const Mapp = dynamic(() => import("@/components/map/index"), { ssr: false });
 
 const Map = () => {
+  const searchBarHeight = 64;
+
   return (
-    <Grid
-      mx="auto"
-      sx={{ p: 1, direction: "rtl", height: "100%" }}>
-      {/* Search component */}
+    <Box
+      sx={{
+        height: "100vh",
+        direction: "rtl",
+        overflow: "hidden",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      {/* Fixed Search */}
       <Box
         sx={{
           position: "fixed",
           top: 0,
-          right: 0,
           left: 0,
+          right: 0,
           zIndex: 9,
           py: 2,
           px: { xs: 1, md: 10, lg: 15 },
           bgcolor: "#fff",
-          justifyContent: "start",
-          mx: "auto",
           boxShadow: "0px 0px 5px #00000040",
-        }}>
+        }}
+      >
         <Search />
       </Box>
 
-      <Grid mt={12} sx={{height: "100%" }} >
+      {/* Map Wrapper */}
+      <Box
+        sx={{
+          mt: `${searchBarHeight}px`,
+          height: `calc(100vh - ${searchBarHeight}px)`,
+          flex: 1,
+          overflow: "hidden",
+        }}
+      >
         <Mapp />
-      </Grid>
-    </Grid>
+      </Box>
+    </Box>
   );
 };
 
